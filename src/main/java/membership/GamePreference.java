7dbbd323acbac2d30c68f5523dd57188c7a9892f
Tamir <tamirhaldlevi@gmail.com>
2025-11-20 11:05:24 +0100

    GamePreference og PlayPreference skulle meget gerne være sat op korrekt nu med getters + setters + toString()

--- src/main/java/membership/GamePreference.java
+++ src/main/java/membership/GamePreference.java
@@ -1,4 +1,7 @@
 package main.java.membership;
 
 public enum GamePreference {
+    SINGLE,
+    DOUBLE,
+    MIXDOUBLE
 }
--- src/main/java/membership/PlayPreference.java
+++ src/main/java/membership/PlayPreference.java
@@ -1,4 +1,44 @@
 package main.java.membership;
 
+import java.util.HashSet;
+
 public class PlayPreference {
+    private boolean isCompetetiveMember;
+    private HashSet<GamePreference> gamePreference;
+    private int smashPoint;
+
+    public PlayPreference(boolean isCompetetiveMember, HashSet<GamePreference> gamePreference,int smashPoint) {
+        this.isCompetetiveMember = isCompetetiveMember;
+        this.gamePreference = gamePreference;
+        this.smashPoint = smashPoint;
+    }
+
+    public boolean isCompetetiveMember() {
+        return isCompetetiveMember;
+    }
+
+    public HashSet<GamePreference> getGamePreference() {
+        return gamePreference;
+    }
+
+    public int getSmashPoint() {
+        return smashPoint;
+    }
+
+    public void setCompetetiveMember(boolean competetiveMember) {
+        this.isCompetetiveMember = competetiveMember;
+    }
+
+    public void setGamePreference(HashSet<GamePreference> gamePreference) {
+        this.gamePreference = gamePreference;
+    }
+
+    public void setSmashPoint(int smashPoint) {
+        this.smashPoint = smashPoint;
+    }
+
+    @Override
+    public String toString(){
+        return "Konkurrencespiller: " + isCompetetiveMember + "Disciplin(er): " + gamePreference + "SMASH-Point: " + smashPoint;
+    }
 }
