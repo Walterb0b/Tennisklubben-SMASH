79b0516ac061073cb0f009274f0602bd2c40af1c
Walterb0b <osca0001@stud.ek.dk>
2025-11-21 14:31:05 +0100

    Unit test på AcitveMembership metode(r)

--- test/main/java/membership/ActiveMembershipTest.java
+++ test/main/java/membership/ActiveMembershipTest.java
@@ -2,12 +2,21 @@ package main.java.membership;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ActiveMembershipTest {
 
+    Member m1 = new Member("John Doe", 12345678, LocalDate.of(2008, 9, 23), new ActiveMembership());
+
     @Test
-    void calculateYearlyFee() {
-        assertEquals(800, 18);
+    void calculateYearlyFeeSucces() {
+        assertEquals(800, m1.getMembership().calculateYearlyFee());
+    }
+
+    @Test
+    void calculateYearlyFeeFail() {
+        assertEquals(1500, m1.getMembership().calculateYearlyFee());
     }
 }
