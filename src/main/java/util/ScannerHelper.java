de7e69c1035d496ad3e00dd72b1e5be2b29992d2
Walterb0b <osca0001@stud.ek.dk>
2025-11-24 11:23:35 +0100

    De første menuer er lavet

--- src/main/java/util/ScannerHelper.java
+++ src/main/java/util/ScannerHelper.java
@@ -10,6 +10,41 @@ public class ScannerHelper {
     public ScannerHelper () {
         this.sc = new Scanner(System.in);
     }
+    public void printMainMenu(){
+        System.out.println("=== Hovedmenu ===");
+        System.out.println("1. Medlemskab");
+        System.out.println("2. Økonomi");
+        System.out.println("3. Spillerstatistik");
+        System.out.println("4. Træning -og turneringsdata");
+        System.out.println("0. Luk programmet");
+    }
+
+    public void printFinanceMenu(){
+        System.out.println("=== Økonomi ===");
+        System.out.println("1. Tilføj indbetaling");
+        System.out.println("2. Restance liste");
+        System.out.println("3. Se forventede kontingent betalinger");
+        System.out.println("0. Gå tilbage");
+    }
+
+    public void printStatsMenu(){
+        System.out.println("=== Spillerstatistik ===");
+        System.out.println("1. Se top 5 spillere");
+        System.out.println("2. Se statistik for en spiller");
+        System.out.println("3. Se turneringskampe for spiller");
+        System.out.println("0. Gå tilbage");
+    }
+
+    public void printResultMenu(){
+        System.out.println("=== Træning og turneringsdata ===");
+        System.out.println("1. Tilføj turneringskamp");
+        System.out.println("2. Rediger turneringskamp");
+        System.out.println("3. Slet turneringskamp");
+        System.out.println("4. Tilføj træningsresultat");
+        System.out.println("5. Rediger træningsresultat");
+        System.out.println("6. Slet træningsresultat");
+        System.out.println("0. Gå tilbage");
+    }
 
     //Metode til at få int fra scanner input
     public int askNumber(int intMax) {
