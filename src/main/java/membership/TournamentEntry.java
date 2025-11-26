6b7a22e57aae6fb9b487e5c3eaae7e9233650700
lukasma02 <andersenlukas02@gmail.com>
2025-11-26 10:09:03 +0100

    tournament + entry

---
+++ src/main/java/membership/TournamentEntry.java
@@ -0,0 +1,9 @@
+package main.java.membership;
+
+public class TournamentEntry {
+
+    private int entryID;
+
+    public TournamentEntry(int entryID){this.entryID = entryID;}
+    public int getEntryID(){return entryID;}
+}
