1637f48b9dd42d94ef38d909f98645e755b83ff5
lindkvst <lindkviste@gmail.com>
2025-11-27 10:34:12 +0100

    MembershipPayment

---
+++ src/main/java/finance/MembershipPayment.java
@@ -0,0 +1,14 @@
+package main.java.finance;
+
+import main.java.membership.Member;
+
+import java.time.LocalDate;
+
+public class MembershipPayment {
+    private int paymentID;
+    private LocalDate payedDate;
+    private String seasonQuarter;
+    private Member member;
+    private Double amount;
+
+}
